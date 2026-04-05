6cf4d61a098a1890bbcb7cdadcdc708a1243b943
Seu Nome <seu.email@example.com>
2026-04-05 10:59:43 -0300

    Alteraçoes no diretorio

---
+++ script.js
@@ -0,0 +1,16 @@
+const roleta = document.getElementById("roleta");
+const botao = document.getElementById("girar");
+const resultado = document.getElementById("resultado");
+
+const opcoes = ["A", "B", "C", "D", "E", "F"];
+
+botao.addEventListener("click", () => {
+  const index = Math.floor(Math.random() * opcoes.length);
+  const angulo = 360 * 5 + (index * (360 / opcoes.length));
+
+  roleta.style.transform = `rotate(${angulo}deg)`;
+
+  setTimeout(() => {
+    resultado.textContent = "Resultado: " + opcoes[index];
+  }, 2000);
+});
